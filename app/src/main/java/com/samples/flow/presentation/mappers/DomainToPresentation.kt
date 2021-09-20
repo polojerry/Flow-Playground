31d886b95f377d11ed97c9c67426dc2878904f7d
polojerry <polojerry4@gmail.com>
2021-09-20 08:58:35 +0300

    added presentation implementation of movies

--- app/src/main/java/com/samples/flow/presentation/mappers/DomainToPresentation.kt
+++ app/src/main/java/com/samples/flow/presentation/mappers/DomainToPresentation.kt
@@ -2,8 +2,10 @@ package com.samples.flow.presentation.mappers
 
 import com.samples.flow.domain.models.Friend
 import com.samples.flow.domain.models.Game
+import com.samples.flow.domain.models.Movie
 import com.samples.flow.presentation.models.FriendPresenter
 import com.samples.flow.presentation.models.GamePresenter
+import com.samples.flow.presentation.models.MoviePresenter
 
 fun Game.toPresentation(): GamePresenter {
     return GamePresenter(
@@ -13,8 +15,16 @@ fun Game.toPresentation(): GamePresenter {
         id = id
     )
 }
+
 fun Friend.toPresentation(): FriendPresenter {
     return FriendPresenter(
         name = name
+    )
+}
+
+fun Movie.toPresentation(): MoviePresenter {
+    return MoviePresenter(
+        title = title,
+        imageUrl = imageUrl
     )
 }
