83757688e1de7143f3020197e0d819ebbe36c962
polojerry <polojerry4@gmail.com>
2021-09-20 08:12:31 +0300

    added data-remote implementation of game

---
+++ app/src/main/java/com/samples/flow/data/remote/models/GameNetwork.kt
@@ -0,0 +1,8 @@
+package com.samples.flow.data.remote.models
+
+data class GameNetwork(
+    val name: String = "",
+    val genre: String = "",
+    val image_url: String = "",
+    val id :String = ""
+)
